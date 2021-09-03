bfac3325d83ce493b51ab8602a6a7a22d5f4201d
Henry Nawrocki <hdev@sztanski.com>
2021-09-02 19:11:08 -0500

    create TagCloud module

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="tag"></div>
+  <div class="tag">
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="postsWithTag" />
+    </div>
+    <div v-else>
+      <Spinner />
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import Spinner from '../components/Spinner.vue'
+import PostList from '../components/PostList.vue'
 import getPosts from '../composables/getPosts'
+
 export default {
+  components: { PostList, Spinner },
   setup() {
+    const route = useRoute()
     const { posts, error, load } = getPosts()
+    console.log(route.params.tag)
 
     load()
+
+    const postsWithTag = computed(() => {
+      return posts.value.filter((post) => post.tags.includes(route.params.tag))
+    })
+
+    return { error, posts, postsWithTag }
   }
 }
 </script>
